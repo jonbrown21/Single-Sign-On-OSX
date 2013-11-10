79269894c5bb6eae4bfb34368812fd2f0614b566
Jon Brown <jonbrown2@mac.com>
2013-11-09 19:35:04 -0500

    Changes

--- Bind-Installer/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "What would you like to do (bind/unbind/add)? "
-
-[ "$REPLY" != "bind" ] || exec /Bind-Installer/src/bind.sh
-[ "$REPLY" != "unbind" ] || exec /Bind-Installer/src/unbind.sh
-[ "$REPLY" != "add" ] || exec /Bind-Installer/src/add.sh
- 
